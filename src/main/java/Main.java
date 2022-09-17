75aff152d237a7760973f60f73fd38180780e369
200MB <notrealxsdenied3435@gmail.com>
2022-09-17 16:44:18 +0400

    demo code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,11 @@
-import crawlers.MyfoodbookURLGen;
+import crawlers.MyFoodBookClass;
 
 public class Main {
     public static void main(String[] args) {
-
-//        Myfoodbook crawler = new Myfoodbook("https://myfoodbook.com.au/recipes/show/eggplant-lime-and-tofu-laksa");
-//        System.out.println(crawler.getName());
-//        System.out.println(crawler.getIngredients());
-//        System.out.println(crawler.getMethod());
-// -----------------------------------------------------------scrapper demo
-        MyfoodbookURLGen crawler2 = new MyfoodbookURLGen();
-        crawler2.startThreads();
-
-//        Connection con = DataConnect.getConnection();
-// -----------------------------------------------------------sql demo
-
+        MyFoodBookClass myFoodBookClass = new MyFoodBookClass();
+        myFoodBookClass.startThreads();
     }
 
 }
-/*
-    ///////////////////////////////////////////////////////////////////////////
-    //                                box                                    //
-    ///////////////////////////////////////////////////////////////////////////
-                           ^^^this box is very cool^^^
-*/
+
 
